53a25fcf82f00c29be920ada81e99141e989f687
Francesc Brau <frjosepgbrau@gmail.com>
2025-12-05 23:13:35 +0400

    Test Commit

--- src/com/grupo4/main/Test.java
+++ src/com/grupo4/main/Test.java
@@ -1,6 +1,11 @@
 package com.grupo4.main;
 
-public class Test {
+public class Test { 
+    // Aixo es una prova de comentari
+
+    /*Volem veure com funciona el github i el control de versions
+    
+Estem fent comments */
     public static void main(String[] args) {
         CEC cec = new CEC();
                cec.menuPrincipal();
